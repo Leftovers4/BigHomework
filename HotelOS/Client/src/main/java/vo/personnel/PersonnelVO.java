88ba308f2056ec59698924be717db417b23ab298
Aneureka <aneureka2@gmail.com>
2016-11-09 20:34:02 +0800

    abit

--- HotelOS/Client/src/main/java/vo/personnel/PersonnelVO.java
+++ HotelOS/Client/src/main/java/vo/personnel/PersonnelVO.java
@@ -8,32 +8,17 @@ public class PersonnelVO {
     /**
      * 工作人员ID
      */
-    private long personnelID;
+    public long personnelID;
 
     /**
      * 密码
      */
-    private String password;
+    public String password;
 
     public PersonnelVO(long personnelID, String password) {
         this.personnelID = personnelID;
         this.password = password;
     }
 
-    public long getPersonnelID() {
-        return personnelID;
-    }
-
-    public void setPersonnelID(long personnelID) {
-        this.personnelID = personnelID;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
 }
