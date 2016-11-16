f85f923a3c72b5fdae27fd8f35b6b1e2e8255b7f
JasonCarryU <734779745@qq.com>
2016-11-16 21:16:35 +0800

    注释掉错误代码

--- HotelOS/Client/src/main/java/presentation/justfortest/OrderListScene.java
+++ HotelOS/Client/src/main/java/presentation/justfortest/OrderListScene.java
@@ -1,31 +1,32 @@
-/**
- * Created by:Hitiger
- * Date: 2016/11/5   Time: 20:26
- * Description:
- */
-
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-
-public class OrderListScene extends Application {
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) throws IOException {
-        FXMLLoader fxmlLoader=new FXMLLoader(getClass().getResource("orderListSceneFxml.fxml"));
-        Parent root= fxmlLoader.load();
-        Controller controller = fxmlLoader.getController();
-        controller.setStage(primaryStage);
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
-    }
-}
+///**
+// * Created by:Hitiger
+// * Date: 2016/11/5   Time: 20:26
+// * Description:
+// */
+//
+//import javafx.application.Application;
+//import javafx.fxml.FXMLLoader;
+//import javafx.scene.Parent;
+//import javafx.scene.Scene;
+//import javafx.stage.Stage;
+//
+//import java.io.IOException;
+//
+//public class OrderListScene extends Application {
+//
+//    public static void main(String[] args) {
+//        launch(args);
+//    }
+//
+//    @Override
+//    public void start(Stage primaryStage) throws IOException {
+//        FXMLLoader fxmlLoader=new FXMLLoader(getClass().getResource("orderListSceneFxml.fxml"));
+//        Parent root= fxmlLoader.load();
+//        Controller controller = fxmlLoader.getController();
+//        controller.setStage(primaryStage);
+//        primaryStage.setTitle("Hello World");
+//        primaryStage.setScene(new Scene(root));
+//        primaryStage.show();
+//    }
+//}
+////
