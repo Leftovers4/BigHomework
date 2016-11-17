247ed3c75d8feb1b72885b9bc2192ef91fd1e4e9
Aquarius-Wu <wuyoujie0208@outlook.com>
2016-11-18 00:54:32 +0800

    user界面雏形，左边栏按钮特效和监听以及界面基本配色

---
+++ HotelOS/Client/src/main/java/presentation/userui/usercontroller/UserSceneController.java
@@ -0,0 +1,65 @@
+package presentation.userui.usercontroller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+/**
+ * Created by wyj on 2016/11/17.
+ */
+public class UserSceneController {
+
+    @FXML private Button indexBtn;
+    @FXML private Button userInfoBtn;
+    @FXML private Button orderListBtn;
+    @FXML private Button searchHotelBtn;
+    @FXML private Button hotelRegisteredBtn;
+    @FXML private ImageView leftBarSlider;
+
+    Stage stage;
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public void index() {
+        leftBarSlider.setVisible(false);
+    }
+    public void userInfo() {
+        leftBarSlider.setVisible(true);
+        leftBarSlider.setLayoutY(260);
+        leftBarSlider.setLayoutX(193);
+    }
+
+    public void orderList() {
+        leftBarSlider.setVisible(true);
+        leftBarSlider.setLayoutY(305);
+        leftBarSlider.setLayoutX(193);
+    }
+
+    public void searchHotel() {
+        leftBarSlider.setVisible(true);
+        leftBarSlider.setLayoutY(350);
+        leftBarSlider.setLayoutX(193);
+    }
+
+    public void hotelRegistered() {
+        leftBarSlider.setVisible(true);
+        leftBarSlider.setLayoutY(395);
+        leftBarSlider.setLayoutX(193);
+    }
+
+    /**
+     * 关闭窗口
+     */
+    public void closeWindow() {
+        stage.close();
+    }
+
+    /**
+     * 最小化窗口
+     */
+    public void minimizeWindow() {
+        stage.setIconified(true);
+    }
+}
--- HotelOS/Client/src/main/java/presentation/util/CommonController.java
+++ HotelOS/Client/src/main/java/presentation/util/CommonController.java
@@ -1,6 +1,7 @@
 package presentation.util;
 
 import javafx.fxml.FXML;
+import javafx.stage.Stage;
 
 /**
  * Created by Hitiger on 2016/11/16.
@@ -8,17 +9,26 @@ import javafx.fxml.FXML;
  */
 public class CommonController {
 
+    Stage stage;
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
     /**
      * 关闭窗口
      */
     @FXML
-    protected void closeWindow(){}
+    protected void closeWindow(){
+        stage.close();
+    }
 
     /**
      * 最小化窗口
      */
     @FXML
-    protected void minWindow(){}
+    protected void minWindow(){
+        stage.setIconified(true);
+    }
 
     /**
      * 返回上一步操作界面
