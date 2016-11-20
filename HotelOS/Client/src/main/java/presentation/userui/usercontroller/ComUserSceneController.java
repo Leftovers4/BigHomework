de749bd09c9a0eb6ce3949c3710bf1c6a5275907
Aquarius-Wu <wuyoujie0208@outlook.com>
2016-11-20 14:56:45 +0800

    usercommen界面特效修改

--- HotelOS/Client/src/main/java/presentation/userui/usercontroller/ComUserSceneController.java
+++ HotelOS/Client/src/main/java/presentation/userui/usercontroller/ComUserSceneController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import presentation.userui.userscene.InfoPane;
 import presentation.userui.userscene.SearchHotelPane;
@@ -23,6 +24,7 @@ public class ComUserSceneController {
     @FXML private Button searchHotelBtn;
     @FXML private Button hotelRegisteredBtn;
     @FXML private ImageView leftBarSlider;
+    @FXML private Button currentBtn = null;
 
     public void launch(Stage primaryStage){
         mainPane.getChildren().add(new InfoPane(primaryStage));
@@ -32,18 +34,35 @@ public class ComUserSceneController {
     @FXML
     private void index() {
         leftBarSlider.setVisible(false);
+        userInfoBtn.setStyle("-fx-background-color: transparent");
+        orderListBtn.setStyle("-fx-background-color: transparent");
+        searchHotelBtn.setStyle("-fx-background-color: transparent");
+        hotelRegisteredBtn.setStyle("-fx-background-color: transparent");
+        currentBtn = indexBtn;
     }
     @FXML
     private void userInfo() {
         leftBarSlider.setVisible(true);
         leftBarSlider.setLayoutY(260);
         leftBarSlider.setLayoutX(193);
+        mainPane.getChildren().remove(0);
+        mainPane.getChildren().add(new InfoPane(stage));
+        userInfoBtn.setStyle("-fx-background-color: #0F81C7");
+        orderListBtn.setStyle("-fx-background-color: transparent");
+        searchHotelBtn.setStyle("-fx-background-color: transparent");
+        hotelRegisteredBtn.setStyle("-fx-background-color: transparent");
+        currentBtn = userInfoBtn;
     }
     @FXML
     private void orderList() {
         leftBarSlider.setVisible(true);
         leftBarSlider.setLayoutY(305);
         leftBarSlider.setLayoutX(193);
+        orderListBtn.setStyle("-fx-background-color: #0F81C7");
+        userInfoBtn.setStyle("-fx-background-color: transparent");
+        searchHotelBtn.setStyle("-fx-background-color: transparent");
+        hotelRegisteredBtn.setStyle("-fx-background-color: transparent");
+        currentBtn = orderListBtn;
     }
     @FXML
     private void searchHotel() {
@@ -52,11 +71,101 @@ public class ComUserSceneController {
         leftBarSlider.setLayoutX(193);
         mainPane.getChildren().remove(0);
         mainPane.getChildren().add(new SearchHotelPane(stage));
+        searchHotelBtn.setStyle("-fx-background-color: #0F81C7");
+        userInfoBtn.setStyle("-fx-background-color: transparent");
+        orderListBtn.setStyle("-fx-background-color: transparent");
+        hotelRegisteredBtn.setStyle("-fx-background-color: transparent");
+        currentBtn = searchHotelBtn;
     }
     @FXML
     private void hotelRegistered() {
         leftBarSlider.setVisible(true);
         leftBarSlider.setLayoutY(395);
         leftBarSlider.setLayoutX(193);
+        hotelRegisteredBtn.setStyle("-fx-background-color: #0F81C7");
+        userInfoBtn.setStyle("-fx-background-color: transparent");
+        orderListBtn.setStyle("-fx-background-color: transparent");
+        searchHotelBtn.setStyle("-fx-background-color: transparent");
+        currentBtn = hotelRegisteredBtn;
+    }
+
+    @FXML
+    private void mouseOnUserInfo() {
+        userInfoBtn.setStyle("-fx-background-color: deepskyblue");
+        if(orderListBtn != currentBtn) {
+            orderListBtn.setStyle("-fx-background-color: transparent");
+        } else {
+            orderListBtn.setStyle("-fx-background-color: #0F81C7");
+        }
+        if(searchHotelBtn != currentBtn) {
+            searchHotelBtn.setStyle("-fx-background-color: transparent");
+        } else {
+            searchHotelBtn.setStyle("-fx-background-color: #0F81C7");
+        }
+        if(hotelRegisteredBtn != currentBtn) {
+            hotelRegisteredBtn.setStyle("-fx-background-color: transparent");
+        } else {
+            hotelRegisteredBtn.setStyle("-fx-background-color: #0F81C7");
+        }
+    }
+
+    @FXML
+    private void mouseOnorderList() {
+        orderListBtn.setStyle("-fx-background-color: deepskyblue");
+        if(userInfoBtn != currentBtn) {
+            userInfoBtn.setStyle("-fx-background-color: transparent");
+        } else {
+            userInfoBtn.setStyle("-fx-background-color: #0F81C7");
+        }
+        if(searchHotelBtn != currentBtn) {
+            searchHotelBtn.setStyle("-fx-background-color: transparent");
+        } else {
+            searchHotelBtn.setStyle("-fx-background-color: #0F81C7");
+        }
+        if(hotelRegisteredBtn != currentBtn) {
+            hotelRegisteredBtn.setStyle("-fx-background-color: transparent");
+        } else {
+            hotelRegisteredBtn.setStyle("-fx-background-color: #0F81C7");
+        }
+    }
+
+    @FXML
+    private void mouseOnSearchHotel() {
+        searchHotelBtn.setStyle("-fx-background-color: deepskyblue");
+        if(userInfoBtn != currentBtn) {
+            userInfoBtn.setStyle("-fx-background-color: transparent");
+        } else {
+            userInfoBtn.setStyle("-fx-background-color: #0F81C7");
+        }
+        if(orderListBtn != currentBtn) {
+            orderListBtn.setStyle("-fx-background-color: transparent");
+        } else {
+            orderListBtn.setStyle("-fx-background-color: #0F81C7");
+        }
+        if(hotelRegisteredBtn != currentBtn) {
+            hotelRegisteredBtn.setStyle("-fx-background-color: transparent");
+        } else {
+            hotelRegisteredBtn.setStyle("-fx-background-color: #0F81C7");
+        }
+    }
+
+    @FXML
+    private void mouseOnRegisteredHotel() {
+        hotelRegisteredBtn.setStyle("-fx-background-color: deepskyblue");
+        if(userInfoBtn != currentBtn) {
+            userInfoBtn.setStyle("-fx-background-color: transparent");
+        } else {
+            userInfoBtn.setStyle("-fx-background-color: #0F81C7");
+        }
+        if(orderListBtn != currentBtn) {
+            orderListBtn.setStyle("-fx-background-color: transparent");
+        } else {
+            orderListBtn.setStyle("-fx-background-color: #0F81C7");
+        }
+        if(searchHotelBtn != currentBtn) {
+            searchHotelBtn.setStyle("-fx-background-color: transparent");
+        } else {
+            searchHotelBtn.setStyle("-fx-background-color: #0F81C7");
+        }
     }
 }
