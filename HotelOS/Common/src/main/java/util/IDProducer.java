b669c3fe0ddc4af8c7b8df690fce5e762e730bd0
kevin807210561 <lzh15917953854@163.com>
2016-12-05 13:28:55 +0800

    又改

--- HotelOS/Common/src/main/java/util/IDProducer.java
+++ HotelOS/Common/src/main/java/util/IDProducer.java
@@ -1,5 +1,6 @@
 package util;
 
+import java.time.LocalDate;
 import java.util.Random;
 
 /**
@@ -8,10 +9,17 @@ import java.util.Random;
  */
 public class IDProducer {
 
-    private static final int MAX_HOTELID = 999998;
-    private static final int MAX_GENERALID = 999999;
+    private static final int MAX_HOTELID = 999999;
+
+    private static final int MAX_GENERALID = 1000000;
+
     private static final int HOTELID_FOR_WP = 999999;
+
     private static final int MIN_GENERALID = 100000;
+
+    private static final int MAX_ORDERID = 1000;
+
+    private static final int MIN_ORDERID = 100;
 
     public static int produceHotelID(){
         return new Random().nextInt(MAX_HOTELID - MIN_GENERALID) + MIN_GENERALID;
@@ -22,5 +30,18 @@ public class IDProducer {
     }
 
     public static int produceHotelIDForWP(){return HOTELID_FOR_WP;}
+
+    public static String produceOrderID(long hotelID){
+        String res = "";
+
+        res += hotelID;
+        LocalDate today = LocalDate.now();
+        res += today.getYear();
+        res += today.getMonth();
+        res += today.getDayOfMonth();
+        res += new Random().nextInt(MAX_ORDERID - MIN_ORDERID) + MIN_ORDERID;
+
+        return res;
+    }
 
 }
