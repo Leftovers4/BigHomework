a8e0436c6643b0d1192c1ebae7c5baf85be457cd
Aneureka <aneureka2@gmail.com>
2016-12-05 23:20:55 +0800

    小修改

---
+++ HotelOS/Server/src/main/java/main/ServerLauncher.java
@@ -0,0 +1,16 @@
+package main;
+
+import rmi.RemoteHelper;
+
+/**
+ * Created by Hiki on 12/5/2016.
+ */
+public class ServerLauncher {
+
+    public static void main(String[] args) {
+        new RemoteHelper().run();
+    }
+
+
+}
+
