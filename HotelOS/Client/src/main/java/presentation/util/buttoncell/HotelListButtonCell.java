661a338ead7846f3c5661ad2efa217d350ef9201
JasonCarryU <734779745@qq.com>
2016-12-08 23:55:14 +0800

    修改列表按钮

--- HotelOS/Client/src/main/java/presentation/util/buttoncell/HotelListButtonCell.java
+++ HotelOS/Client/src/main/java/presentation/util/buttoncell/HotelListButtonCell.java
@@ -19,51 +19,14 @@ import vo.order.OrderVO;
  * Description :
  */
 public class HotelListButtonCell extends TableCell<OrderVO, Boolean> {
-    final private HBox btnBox = new HBox();
-    final private Button detailButton = new Button();
-    final private Button checkInButton = new Button();
-    final private Button checkOutButton = new Button();
+
     private TableView tableView;
+    private Pane mainPane;
 
     public HotelListButtonCell(final Pane mainPane, final TableView tableView) {
+        this.mainPane = mainPane;
         this.tableView = tableView;
-
         this.getStylesheets().add(HotelListButtonCell.class.getResource("/css/hotelworker/hotelworkerstyle.css").toExternalForm());
-        Image detailImage = new Image("/img/hotelworker/checkorderdetail.png");
-        detailButton.setGraphic(new ImageView(detailImage));
-        detailButton.getStyleClass().add("TableButtonCell");
-
-        Image checkInImage = new Image("/img/hotelworker/checkin.png");
-        checkInButton.setGraphic(new ImageView(checkInImage));
-        checkInButton.getStyleClass().add("TableButtonCell");
-
-        Image checkOutImage = new Image("/img/hotelworker/checkout.png");
-        checkOutButton.setGraphic(new ImageView(checkOutImage));
-        checkOutButton.getStyleClass().add("TableButtonCell");
-
-        detailButton.setOnAction(event -> {
-            int selectedIndex = getTableRow().getIndex();
-            OrderVO orderVO = (OrderVO) tableView.getItems().get(selectedIndex);
-            mainPane.getChildren().clear();
-            mainPane.getChildren().add(new OrderDetailPane(mainPane, false,true,orderVO));
-        });
-
-        checkInButton.setOnAction(event -> {
-            int selectedIndex = getTableRow().getIndex();
-            OrderVO orderVO = (OrderVO) tableView.getItems().get(selectedIndex);
-            mainPane.getChildren().clear();
-            mainPane.getChildren().add(new UpdateOrderInfoPane(mainPane, true, true, orderVO));
-        });
-
-        checkOutButton.setOnAction(event -> {
-            int selectedIndex = getTableRow().getIndex();
-            OrderVO orderVO = (OrderVO) tableView.getItems().get(selectedIndex);
-            mainPane.getChildren().clear();
-            mainPane.getChildren().add(new UpdateOrderInfoPane(mainPane, false, true, orderVO));
-        });
-
-        btnBox.setSpacing(10);
-        btnBox.setAlignment(Pos.CENTER);
     }
 
     @Override
@@ -73,21 +36,72 @@ public class HotelListButtonCell extends TableCell<OrderVO, Boolean> {
             setGraphic(null);
             setText(null);
         } else {
+            HBox btnBox = new HBox();
+            Button detailButton = new Button();
+
+            Image detailImage = new Image("/img/hotelworker/checkorderdetail.png");
+            detailButton.setGraphic(new ImageView(detailImage));
+            detailButton.getStyleClass().add("TableButtonCell");
+
+            detailButton.setOnAction(event -> {
+                int selectedIndex = getTableRow().getIndex();
+                OrderVO orderVO = (OrderVO) tableView.getItems().get(selectedIndex);
+                mainPane.getChildren().clear();
+                mainPane.getChildren().add(new OrderDetailPane(mainPane, false,true,orderVO));
+            });
+
+            btnBox.setSpacing(10);
+            btnBox.setAlignment(Pos.CENTER);
             btnBox.getChildren().clear();
+
             OrderVO temp = (OrderVO)tableView.getItems().get(getTableRow().getIndex());
+
             if(temp.orderType == OrderType.Executed){
-                if(temp.orderTimeVO.leaveTime == null){
+                if(temp.orderTimeVO.leaveTime != null){
                     btnBox.setAlignment(Pos.CENTER_LEFT);
                     btnBox.setPadding(new Insets(0,0,0,13));
                     btnBox.getChildren().add(detailButton);
                 }else {
+                    Button checkOutButton = new Button();
+
+                    Image checkOutImage = new Image("/img/hotelworker/checkout.png");
+                    checkOutButton.setGraphic(new ImageView(checkOutImage));
+                    checkOutButton.getStyleClass().add("TableButtonCell");
+
+                    checkOutButton.setOnAction(event -> {
+                        int selectedIndex = getTableRow().getIndex();
+                        OrderVO orderVO = (OrderVO) tableView.getItems().get(selectedIndex);
+                        mainPane.getChildren().clear();
+                        mainPane.getChildren().add(new UpdateOrderInfoPane(mainPane, false, true, orderVO));
+                    });
+
                     btnBox.setPadding(new Insets(0,5,0,20));
                     btnBox.getChildren().addAll(detailButton, checkOutButton);
                 }
             }else {
-                btnBox.setPadding(new Insets(0,5,0,20));
-                btnBox.getChildren().addAll(detailButton, checkInButton);
+                if(temp.orderType == OrderType.Canceled){
+                    btnBox.setAlignment(Pos.CENTER_LEFT);
+                    btnBox.setPadding(new Insets(0,0,0,13));
+                    btnBox.getChildren().add(detailButton);
+                }else {
+                    Button checkInButton = new Button();
+
+                    Image checkInImage = new Image("/img/hotelworker/checkin.png");
+                    checkInButton.setGraphic(new ImageView(checkInImage));
+                    checkInButton.getStyleClass().add("TableButtonCell");
+
+                    checkInButton.setOnAction(event -> {
+                        int selectedIndex = getTableRow().getIndex();
+                        OrderVO orderVO = (OrderVO) tableView.getItems().get(selectedIndex);
+                        mainPane.getChildren().clear();
+                        mainPane.getChildren().add(new UpdateOrderInfoPane(mainPane, true, true, orderVO));
+                    });
+
+                    btnBox.setPadding(new Insets(0,5,0,20));
+                    btnBox.getChildren().addAll(detailButton, checkInButton);
+                }
             }
+
             setGraphic(btnBox);
             setText(null);
         }
